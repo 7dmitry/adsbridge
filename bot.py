0061434f3c4ad7c483da0c7f8a4e4f200b2009fc
Dima <poletime111@gmail.com>
2026-08-25 23:34:54 +0500

    fix db connection

--- bot.py
+++ bot.py
@@ -41,6 +41,7 @@ DB_USER      = os.getenv("DB_USER")
 DB_PASSWORD  = os.getenv("DB_PASSWORD")
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+crypto_pay = CryptoPay(token=BOT_PAY, network=Networks.TEST_NET)
 dp  = Dispatcher(storage=MemoryStorage())
 
 DATABASE_URL = os.getenv("DATABASE_URL")
